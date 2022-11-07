667244527c350417f0b8ad30895e98fa02a0b8fc
DESKTOP-GV962H6\sinke <sinkevich.work@gmail.com>
2022-11-07 21:40:02 +0300

    task done

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int maxForTwo = 0;
+        maxForTwo = first > second ? first: second;
+        int result = 0;
+        result = maxForTwo > third ? maxForTwo: third;
+        System.out.println(result);
+
     }
 }
+
